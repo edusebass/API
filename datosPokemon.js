f9b18a13fa6a3ae05cdf39f9707c971fd714ce79
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-11-07 15:51:54 -0500

    creacion de funciones

---
+++ datosPokemon.js
@@ -0,0 +1,18 @@
+//consultar la API
+const consultarPokemon = (id,number) => {
+}
+
+//generar un pokemon random
+const btnSeleccionar = () => {
+}
+
+//invocar a la funcion
+btnSeleccionar()
+
+//obtnere la referenca del div dsonde se va a pintar los pokemons
+const lista = document.getElementById("listarpokemon")
+
+//presentar los pokemons
+const pintarPokemon = (data, id) => {
+}
+
