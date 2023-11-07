c589d5c81d6db5cb1a7d9e3780b0d419bccacb3d
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-11-07 16:27:06 -0500

    logica para obtner la api

--- datosUsers.js
+++ datosUsers.js
@@ -1,8 +1,17 @@
 let url = "https://jsonplaceholder.typicode.com/users";
 
 const obtenerUsaurios = async () => {
-}
-obtenerUsaurios();
+    try {
+        let response = await fetch(url);
+        if (!response.ok) {
+        throw new Error("Ourrio un Error al realizar la petición");
+        }
+        let data = await response.json();
+        pintarUsuarios(data);
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 const pintarUsuarios = (data) => 
 {
